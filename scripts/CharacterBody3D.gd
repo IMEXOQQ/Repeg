f6cc26a37493135101bfc2d88d447e639b142f6b
IMEXOQQ <imamiev.1255@gmail.com>
2023-12-16 14:32:50 +0300

    version001
    Physics, light fix

--- scripts/CharacterBody3D.gd
+++ scripts/CharacterBody3D.gd
@@ -2,8 +2,7 @@ extends CharacterBody3D
 
 
 const SPEED = 150.0
-const JUMP_VELOCITY = 4.5
-
+const JUMP_VELOCITY = 300.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var target_velocity = Vector3.ZERO
@@ -22,7 +21,7 @@ func _physics_process(delta):
 		target_velocity.y -= gravity * delta
 
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		target_velocity.y = JUMP_VELOCITY
+		target_velocity.y = JUMP_VELOCITY * delta
 
 	if Input.is_action_pressed("ui_right"):
 		direction.x += 1
